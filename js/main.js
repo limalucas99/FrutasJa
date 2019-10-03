b063d1a2a09daf42778d0a3f3456d5f97d73cf70
Lucas Lima <lucaslim.lux@gmail.com>
2019-10-03 03:26:40 -0300

    Finalizando

--- js/main.js
+++ js/main.js
@@ -23,3 +23,37 @@
     
     contentWaypoint()
 })(jQuery)
+
+//div Detalhes
+$('.card').on("mousemove mouseleave", function( event ) {
+       var divid = "#detalhes";
+   if(event.type == "mouseleave"){
+      $(divid).hide();
+   }else{
+      var divid_height = $(divid).height(); // altura da div
+      var scrolTop = $(window).scrollTop(); // scroll da janela
+      $(divid).css({
+         top: event.clientY-divid_height+scrolTop,
+         left: event.clientX+5
+      }).show()
+   }
+ })
+    
+ 
+//fim div detalhes
+//--------------------------------------------------------------------------------------------------//
+ 
+   let navLink = $('.barra-animada');
+    
+    navLink.on('click',function(e){
+        e.preventDefault(); //tiro o comportamento padrão, que é ir direto pra onde cliquei
+          let href = $(this).attr('href'); //pego o elemento href de onde cliquei
+          let alvo = $(href); //alvo, pra onde eu quero ir
+          let pos = alvo.position().top; //pega a posição do top de onde quero ir 
+          
+          $('html, body').animate({scrollTop: pos}, 1500); //animo o scroll
+    })
+
+
+
+//-----------------------------------------------------------//
